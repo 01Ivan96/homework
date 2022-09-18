34a6036be246a191c17e8faace6aa21e331a9e90
Ivan <todoritsa.ivan@yandex.ru>
2022-09-18 23:26:34 +0500

    homework №7

--- Program.cs
+++ Program.cs
@@ -1,80 +1,160 @@
-﻿// Задача №41
-// /*
-Console.Write("Количество вводимых чисел: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-while ( number<=0 ) {
-    Console.Write("Количество вводимых чисел должно быть больше нуля: ");
-    number = Convert.ToInt32(Console.ReadLine());
-}
-
-int count = 0;
-string allNumber = string.Empty;
-for ( int i=0; i<number; i++ ) {
-    Console.Write("Введите число: ");
-    int inputNumber = Convert.ToInt32(Console.ReadLine());
-    if ( inputNumber > 0 ) {
-        count++;
+﻿int[,] CreationMatrix() {
+    Console.Write("Количество строк массива: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    while ( rows < 1 ) {
+        Console.Write("Количество строк должно быть больше нуля: ");
+        rows = Convert.ToInt32(Console.ReadLine());
     }
-    allNumber += inputNumber + " ";
+
+    Console.Write("Количество столбцов массива: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    while ( columns < 1 ) {
+        Console.Write("Количество столбцов должно быть больше нуля: ");
+        columns = Convert.ToInt32(Console.ReadLine());
+    }
+
+    Console.Write("Минимальное возможное значение в массиве: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Максимальное возможное значение в массиве: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+    while ( minValue > maxValue ) {
+        Console.Write($"Максимальное значение в массиве должно быть не меньше минимального ({minValue}): ");
+        maxValue = Convert.ToInt32(Console.ReadLine());
+    }
+
+    int[,] array = new int[rows, columns];
+    InputMatrix(array, minValue, maxValue+1);
+    return array;
 }
-Console.WriteLine($"Количество положительных чисел из ряда ( {allNumber}) равно: {count}");
+
+void InputMatrix(int[,] matrix, int minValue, int maxValue ) {
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(minValue, maxValue);
+    }
+}
+
+
+void PrintMatrix(int[,] matrix) {
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+
+// Задача №47
+// /*
+Console.Write("Количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+while ( rows < 1 ) {
+    Console.Write("Количество строк должно быть больше нуля: ");
+    rows = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("Количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+while ( columns < 1 ) {
+    Console.Write("Количество столбцов должно быть больше нуля: ");
+    columns = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("Минимальное возможное значение в массиве: ");
+double minValue = Convert.ToDouble(Console.ReadLine());
+Console.Write("Максимальное возможное значение в массиве: ");
+double maxValue = Convert.ToDouble(Console.ReadLine());
+while ( minValue > maxValue ) {
+    Console.Write($"Максимальное значение в массиве должно быть не меньше минимального ({minValue}): ");
+    maxValue = Convert.ToDouble(Console.ReadLine());
+}
+
+double[,] array = new double[rows, columns];
+for (int i = 0; i < array.GetLength(0); i++ ) {
+    for (int j = 0; j < array.GetLength(1); j++ ) {
+        array[i, j] = minValue + new Random().NextDouble() * ( Math.Abs(minValue) + maxValue );
+        Console.Write($"{array[i, j]}\t");
+    }
+    Console.WriteLine();
+}
 // */
 
 
 
-// Задача №43
+// Задача №50
 /*
-Console.Write("Первый угловой коэффициент: k1=");
-double k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Первый коэффициент смещения по Oy: b1=");
-double b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Второй угловой коэффициент: k2=");
-double k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Второй коэффициент смещения по Oy: b2=");
-double b2 = Convert.ToDouble(Console.ReadLine());
+int[,] array = CreationMatrix();
+PrintMatrix(array);
 
-double x = ( b2-b1 ) / ( k1-k2 );
-double y = k1 * x + b1;
+Console.Write("Номер строки элемента в массиве: ");
+int numberRow = Convert.ToInt32(Console.ReadLine());
+Console.Write("Номер столбца элемента в массиве: ");
+int numberColumn = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"Для функций y={k1}*x+{b1} и y={k2}*x+{b2} точкой пересечения будет ({x}, {y})");
+if ( numberRow > array.GetLength(0) || numberColumn > array.GetLength(1) ) {
+    Console.Write($"Элемента с индексом ({numberRow-1}; {numberColumn-1}) не существует!");
+} else {
+    Console.Write($"Элемент с индексом ({numberRow-1}; {numberColumn-1}) равен: {array[numberRow-1, numberColumn-1]}");
+}
 */
 
 
 
-// Транспонирование
+// Задача №52
 /*
-Console.Write("Количество строк и столбцов в массиве: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int[,] array = CreationMatrix();
+PrintMatrix(array);
 
-while ( number < 1 ) {
-    Console.Write("Количество строк и столбцов должно быть больше нуля: ");
-    number = Convert.ToInt32(Console.ReadLine());
+for ( int i=0; i<array.GetLength(1); i++ ) {
+    int sum = 0;
+    for (int j = 0; j < array.GetLength(0); j++) {
+        sum += array[j, i];
+    }
+    double arithmeticalMean = sum * Math.Pow(array.GetLength(0), -1);
+    Console.WriteLine($"Среднее арифметическое {i+1} столбца: {arithmeticalMean}");
+}
+*/
+
+
+
+// Задача - "Симпатичный узор"
+/*
+char[,] array = new char[4, 4];
+char[] colorTile = new char[]{'B', 'W'};
+
+for (int i=0; i<array.GetLength(0); i++ ) {
+    for (int j=0; j<array.GetLength(1); j++ ) {
+        array[i, j] = colorTile[new Random().Next(colorTile.Length)];
+    }
 }
 
-Console.WriteLine("");
-Console.WriteLine("Первый массив");
-Console.WriteLine("");
-
-int[,] array = new int[number, number];
-for ( int i=0; i<array.GetLength(0); i++ ) {
-    for ( int k=0; k<array.GetLength(1); k++ ) {
-        array[i, k] = new Random().Next(-100, 101);
-        Console.Write($"{array[i, k]}       ");
+for (int i=0; i<array.GetLength(0); i++ ) {
+    for (int j=0; j<array.GetLength(1); j++ ) {
+        Console.Write($"{array[i, j]} ");        
     }
-    Console.WriteLine("");
+    Console.WriteLine();
 }
 
-Console.WriteLine("");
-Console.WriteLine("Второй массив");
-Console.WriteLine("");
-
-int[,] newArray = new int[array.GetLength(0), array.GetLength(1)];
-for ( int m=0; m<newArray.GetLength(0); m++ ) {
-    for ( int n=0; n<newArray.GetLength(1); n++ ) {
-        newArray[m, n] = array[array.GetLength(1)-1-n, array.GetLength(0)-1-m];
-        Console.Write($"{newArray[m, n]}        ");
+bool beautiful = true;
+for (int i = 0; i<array.GetLength(0)-1; i++) {
+    for (int j = 0; j<array.GetLength(1)-1; j++) {
+        if ( array[i, j] == array[i, j+1] && array[i+1, j] == array[i+1, j+1] && array[i, j] == array[i+1, j+1] ) {
+            beautiful = false;
+            break;
+        }
     }
-    Console.WriteLine("");
+    if ( beautiful == false ) {
+        break;
+    }
+}
+
+Console.WriteLine();
+if ( beautiful == true ) {
+    Console.WriteLine("Узор красивый)");
+} else {
+    Console.WriteLine("Узор некрасивый(");
 }
 */
