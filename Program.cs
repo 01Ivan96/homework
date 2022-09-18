d9de6b18443990f49d78b4859b476fc506a4d02d
Ivan <todoritsa.ivan@yandex.ru>
2022-09-18 23:43:26 +0500

    homework №7

--- Program.cs
+++ Program.cs
@@ -128,12 +128,7 @@ char[] colorTile = new char[]{'B', 'W'};
 for (int i=0; i<array.GetLength(0); i++ ) {
     for (int j=0; j<array.GetLength(1); j++ ) {
         array[i, j] = colorTile[new Random().Next(colorTile.Length)];
-    }
-}
-
-for (int i=0; i<array.GetLength(0); i++ ) {
-    for (int j=0; j<array.GetLength(1); j++ ) {
-        Console.Write($"{array[i, j]} ");        
+        Console.Write($"{array[i, j]} ");
     }
     Console.WriteLine();
 }
